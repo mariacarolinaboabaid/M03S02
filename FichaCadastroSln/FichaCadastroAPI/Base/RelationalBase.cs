78753de033f7ca5704403d06b517fcf8fc0dbab2
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-08-14 15:11:25 -0300

    commit

--- FichaCadastroSln/FichaCadastroAPI/Base/RelationalBase.cs
+++ FichaCadastroSln/FichaCadastroAPI/Base/RelationalBase.cs
@@ -12,6 +12,7 @@ namespace FichaCadastroAPI.Base
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id {get; set; }
+        
         [Required]
         public DateTime DataCadastro {get; set; }   
     }
