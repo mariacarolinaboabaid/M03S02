eea7c381a5f0064ec66c790492063e4e4b132ba3
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-08-14 09:44:09 -0300

    commit

--- FichaCadastroSln/FichaCadastroAPI/Automapper/ConfigurationMapper.cs
+++ FichaCadastroSln/FichaCadastroAPI/Automapper/ConfigurationMapper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using FichaCadastroAPI.DTO.Ficha;
+using FichaCadastroAPI.Models;
 
 namespace FichaCadastroAPI.Automapper
 {
@@ -10,6 +12,19 @@ namespace FichaCadastroAPI.Automapper
     {
         // Construtor
         public ConfigurationMapper(){
+               CreateMap<FichaModel, FichaReadDTO>()
+                 .ForMember(dest => dest.FichaComDetalhes, opt => opt.MapFrom(src => src.Detalhes));
+
+            CreateMap<FichaCreateDTO, FichaModel>()
+                .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.NomeCompleto))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.EmailInformado.ToLower()))
+                .ForMember(dest => dest.DataNascimento, opt => opt.MapFrom(src => src.DataDeNascimento));
+
+            CreateMap<FichaUpdateDTO, FichaModel>();
+
+            CreateMap<DetalheModel, FichaDetalheReadDTO>();
+        }
+    }
 
         }
         
