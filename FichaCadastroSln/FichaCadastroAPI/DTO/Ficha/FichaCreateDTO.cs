5594c85cd446c2cf346bef34e552afaab33abd10
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-08-14 09:34:23 -0300

    fichacreatedto

--- FichaCadastroSln/FichaCadastroAPI/DTO/Ficha/FichaCreateDTO.cs
+++ FichaCadastroSln/FichaCadastroAPI/DTO/Ficha/FichaCreateDTO.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,18 @@ namespace FichaCadastroAPI.DTO.Ficha
 {
     public class FichaCreateDTO
     {
-        public string NomeCompleto {get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [MaxLength(500, ErrorMessage = "Este campo aceita até 500 caracteres")]
+        [MinLength(5, ErrorMessage = "Favor digitar o nome completo")]
+        public string NomeCompleto { get; set; }
 
-        public string EmailInformado {get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Email Inválido")]
+        public string EmailInformado { get; set; }
 
-        public DateTime DataNascimento {get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [Range(typeof(DateTime), "1950-01-01", "2100-12-31", ErrorMessage = "Data inválida")]
+        public DateTime DataDeNascimento { get; set; }
     }
 }
